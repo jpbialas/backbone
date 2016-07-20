Fixed off by one error :/

active_learning.py
@@ -36,7 +36,7 @@ def uncertainty(data, y, k, m, frac_test = 1.0, verbose = True):
 	U = np.where(y < 0)[0]
 	U = np.random.choice(U, frac_test*U.shape[0], replace = False)
 	data_U =  np.take(data, U, axis = 0)
-	L = np.where(y > 0)[0]
+	L = np.where(y >= 0)[0]
 	samples = bootstrap(L, k)
 	X_U = np.zeros(U.shape[0])
 	v_print("Staring model loop", verbose)
@@ -50,7 +50,7 @@ def uncertainty(data, y, k, m, frac_test = 1.0, verbose = True):
 	uncertainties=X_U*(1-X_U)
 	return U[np.argsort(uncertainties)[-m:]]
 
-def trivial_error(X, y, m, frac_test = 1.0):
+def trivial_error(X, y, m, frac_test = 1.0, verbose = True):
 	'''Computes uncertainty from Random Forest's probability metric and returns the m indices
 	of the most uncertain
 	NOTE: This metric is the distance from 50% So larger values are more certain
@@ -61,12 +61,16 @@ def trivial_error(X, y, m, frac_test = 1.0):
 	'''
 	unlabelled = np.where(y < 0)[0]
 	unlabelled = np.random.choice(unlabelled, frac_test*unlabelled.shape[0], replace = False)
-	L = np.where(y > 0)[0]
-	model = RandomForestClassifier(n_estimators = 500, n_jobs = -1, verbose = verbose)
+	L = np.where(y >= 0)[0]
+	model = RandomForestClassifier(n_estimators = 500, n_jobs = -1, verbose = 0)
 	model.fit(X[L], y[L])
-	uncertainties = model.predict_proba(X[unlabelled])[:,1]
-	uncertainties = np.abs(uncertainties-.5)/.5
-	return unlabelled[np.argsort(uncertainties)[:m]]
+	predictions = model.predict_proba(X[unlabelled])
+	if predictions.shape[1]>1:
+		uncertainties = predictions[:,1]
+		uncertainties = np.abs(uncertainties-.5)/.5
+		return unlabelled[np.argsort(uncertainties)[:m]]
+	else:
+		return unlabelled[:m]
 
 
 def test_seg_progress(map_test, base_seg, X_train, X_test, y_train, indices, precision, recall, f1, verbose = True):
@@ -80,14 +84,13 @@ def test_seg_progress(map_test, base_seg, X_train, X_test, y_train, indices, pre
 	recall.append(rec)
 	f1.append(_f1)
 
-def run_active_learning_seg(start_n=1000, step_n=100, boot_n = 100,  n_updates = 100, verbose = True):
+def run_active_learning_seg(start_n=100, step_n=100, boot_n = 100,  n_updates = 100, verbose = True):
 	base_seg = 50
-	X_axis = [0]
+	X_axis = [start_n]
 	map_train, X_train, y_train, names = sc.setup_segs(2, base_seg, [100, 400],.5)
 	map_test, X_test, y_test, _ = sc.setup_segs(3,base_seg, [100,400],  .5)
 
-
-	sample = np.random.random_integers(0,y_train.shape[0]-1, start_n)
+	sample = np.random.choice(np.arange(y_train.shape[0]), start_n, replace = False)
 	random_sample = sample.copy()
 
 	y = np.ones_like(y_train)*-1
@@ -104,21 +107,20 @@ def run_active_learning_seg(start_n=1000, step_n=100, boot_n = 100,  n_updates =
 	graph_p_r = plt.plot(X_axis, prec_r, label = 'Prec Comparison')[0]
 	graph_r_r = plt.plot(X_axis, rec_r, label = 'Rec Comparison')[0]
 	graph_f_r = plt.plot(X_axis, f1_r, label = 'F1 Comparison')[0]
-	#plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-	#   ncol=4, mode="expand", borderaxespad=0.)
-	plt.axis([0, n_updates, 0, 1])
+	plt.axis([start_n, start_n+step_n*n_updates, 0, 1])
 	plt.legend()
 	plt.draw()
 	plt.pause(0.01)
 
 	v_print('starting uncertainty', verbose)
 	for i in range(1, n_updates):
-		X_axis.append(i)
+		X_axis.append(start_n+i*step_n)
 
-		print "there are", np.where(y < 0)[0].shape, "unlabelled"
+		print "there are", np.where(y >= 0)[0].shape[0], "labelled points"
 		#next_indices = uncertainty(X_train, y, boot_n, step_n)
 		next_indices = trivial_error(X_train, y, step_n)
 		sample = np.concatenate((sample, next_indices))
+		y[sample] = y_train[sample]
 		random_sample = np.concatenate((random_sample, strawman_error(X_train, y, boot_n)))
 		test_seg_progress(map_test, base_seg, X_train, X_test, y_train, sample, precision, recall, f1)
 		test_seg_progress(map_test, base_seg, X_train, X_test, y_train, random_sample, prec_r, rec_r, f1_r)
@@ -136,8 +138,6 @@ def run_active_learning_seg(start_n=1000, step_n=100, boot_n = 100,  n_updates =
 		graph_f_r.set_xdata(X_axis)
 		plt.draw()
 		plt.pause(0.01)
-		y = np.ones_like(y_train)*-1
-		y[sample] = y_train[sample]
 
 	plt.waitforbuttonpress()
 