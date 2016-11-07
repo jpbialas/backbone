beaa91d3ac05db8aa6852690456beb21160ac70e
Jared <jaredsfrank@gmail.com>
2016-11-07 14:58:47 -0800

    moved map generation to global

--- james_multilevel_segmentation.py
+++ james_multilevel_segmentation.py
@@ -8,20 +8,22 @@ import datetime
 
 
 def segment_conversion(small, big):
-    small_n = np.max(small)
-    big_n = np.max(big)
+    n = np.max(small)
+    m = np.max(big)
     convert = np.zeros(small_n + 1).astype('int')
     convert[small] = big
     return convert
 
 def main(j):
-    maps = [MapOverlay('datafromjoe/1-0003-0002.tif'),MapOverlay('datafromjoe/1-0003-0003.tif')]
+    print 'here'
     for i in range(10,1010, 10):
         time1 = datetime.datetime.now()
+        print time1
         maps[j].new_segmentation('segmentations/withfeatures{}/shapefilewithfeatures003-00{}-{}.shp'.format(j+2, j+2, i), i)
         time2 = datetime.datetime.now()
         print i, time2-time1
 
+maps = [MapOverlay('datafromjoe/1-0003-0002.tif'),MapOverlay('datafromjoe/1-0003-0003.tif')]
 if __name__ == '__main__':
     Parallel(n_jobs=16)(delayed(main)(i) for i in range(2))
  
