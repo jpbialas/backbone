a9a4fbb4c767570157a0f2d153d84465ff6a0bb6
Jared <jaredsfrank@gmail.com>
2016-11-22 16:30:35 -0800

    fixed display variables

--- noise_progress.py
+++ noise_progress.py
@@ -1,5 +1,5 @@
-#import matplotlib
-#matplotlib.use('Agg')
+import matplotlib
+matplotlib.use('Agg')
 from object_model import ObjectClassifier
 from px_model import PxClassifier
 import analyze_results
